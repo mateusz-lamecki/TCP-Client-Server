8e6ca8a3d94feb63b41d3ed3ab0da6007846263c
Mateusz Lamecki <lamecki.mateusz@gmail.com>
2018-11-16 10:32:08 +0100

    Implement extracting parameters from request

--- server/include/request.h
+++ server/include/request.h
@@ -8,47 +8,55 @@ namespace sk2 {
 
 namespace request {
 
+static const std::string DELIMITER = "@@@";
+
 class Action {
     public:
-    static std::unique_ptr<Action> detect_action(std::string raw_content);
+    static std::unique_ptr<Action> detect_action(std::string raw_action);
     virtual std::string to_string() = 0;
 };
 
 
 class Status {
     public:
-    static std::unique_ptr<Status> detect_status(std::string raw_content);
+    static std::unique_ptr<Status> detect_status(std::string raw_status);
     virtual std::string to_string() = 0;
 };
 
 
 class LoginAction : public Action {
     public:
+    static const int N_PARAMS = 2;
     std::string to_string() override;
 };
 
 class RegisterAction : public Action {
     public:
+    static const int N_PARAMS = 2;
     std::string to_string() override;
 };
 
 class PublishAction : public Action {
     public:
+    static const int N_PARAMS = 3;
     std::string to_string() override;
 };
 
 class SubscribeAction : public Action {
     public:
+    static const int N_PARAMS = 2;
     std::string to_string() override;
 };
 
 class UnsubscribeAction : public Action {
     public:
+    static const int N_PARAMS = 2;
     std::string to_string() override;
 };
 
 class ReadAction : public Action {
     public:
+    static const int N_PARAMS = 1;
     std::string to_string() override;
 };
 
@@ -84,6 +92,12 @@ class InvalidTopicStatus : public Status {
     std::string to_string() override;
 };
 
+class OtherErrorStatus : public Status {
+    public:
+    std::string to_string() override;
+};
+
+
 
 class Response {
     public:
@@ -94,6 +108,7 @@ class Response {
     std::string status_str;
     std::string auxilary_out;
 };
+
 
 
 } // namespace request
