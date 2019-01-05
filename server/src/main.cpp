35b3ea7197ea6a81d4706458f8cdca79c580bf3d
Mateusz Lamecki <lamecki.mateusz@gmail.com>
2019-01-05 10:28:53 +0100

    Pass port in command line args

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <cstdlib>
+
 #include "server_service.h"
 
 int main(int argc, char* argv[]) {
-    sk2::ServerService service(3552);
+    int port = 3552;
+
+    if(argc > 1) {
+        port = strtol(argv[1], &argv[1], 10);
+    }
+
+    sk2::ServerService service(port);
     service.run();
 
     return(0);
